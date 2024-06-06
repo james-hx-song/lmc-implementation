d8775219c85b39bf55734af19887f1c3ca8442b0
James Song <haoxijamessong@gmail.com>
2024-06-05 20:37:39 -0400

    Finished implementation

--- attention.py
+++ attention.py
@@ -10,6 +10,9 @@ learning_rate = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iter = 200
 n_embed = 32
+n_head = 4
+n_layers = 4
+dropout = 0.1
 
 
 # ----------------- Data Preprocessing ----------------- #
@@ -72,6 +75,7 @@ class Head(nn.Module):
     self.query = nn.Linear(n_embed, head_size, bias=False)
     self.value = nn.Linear(n_embed, head_size, bias=False)
     self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+    self.dropout = nn.Dropout(dropout)
 
   def forward(self, x):
     B, T, C = x.shape
@@ -82,10 +86,13 @@ class Head(nn.Module):
     wei = q @ k.transpose(-2, -1) * C**(-0.5) # C is the headsize # (B, T, C) @ (B, C, T) -> (B, T, T)
     wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
     wei = F.softmax(wei, dim=-1) # (B, T, T)
+    wei = self.dropout(wei)
 
     # Perform the weighted aggregation of the values
     v = self.value(x) # (B, T, C)
     out = wei @ v # (B,T, T) @ (B, T, C) -> (B, T, C)
+
+    
 
     return out
 
@@ -96,21 +103,44 @@ class MultiHeadAttention(nn.Module):
   def __init__(self, num_heads, head_size) -> None:
     super().__init__()
     self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+    # Projection
+    self.proj = nn.Linear(n_embed, n_embed)
+    self.dropout = nn.Dropout(dropout)
 
   def forward(self, x):
-    return torch.cat([h(x) for h in self.heads], dim=-1)
+    return self.dropout(self.proj(torch.cat([h(x) for h in self.heads], dim=-1)))
 
 # Implementing Feed-Forward Network
 class FFN(nn.Module):
   def __init__(self, n_embed) -> None:
     super().__init__()
     self.layer = nn.Sequential(
-      nn.Linear(n_embed, n_embed),
+      nn.Linear(n_embed, 4*n_embed),
       nn.ReLU(),
+      nn.Linear(4*n_embed, n_embed), # Projection Layer
+      nn.Dropout(dropout),
     )
   def forward(self, x):
     return self.layer(x)
 
+# Blocks of MultiHeadAttention and FFN
+class Block(nn.Module):
+  ''' Transformer Block'''
+
+  def __init__(self, n_embd, n_head):
+    # n_embd is the embedding dimension, n_head is the number of heads we'd like
+    super().__init__()
+    self.sa = MultiHeadAttention(n_head, n_embd//n_head) # Communication
+    self.ffwd = FFN(n_embd) # Computation
+    self.ln1 = nn.LayerNorm(n_embd)
+    self.ln2 = nn.LayerNorm(n_embd)
+
+  def forward(self, x):
+    # return self.ffwd(self.sa(x))
+    # residual connection
+    x = x + self.sa(self.ln1(x)) # Layer Normalization
+    x = x + self.ffwd(self.ln2(x))
+    return x
 
 class BigramLanguageModel(nn.Module):
 
@@ -121,11 +151,12 @@ class BigramLanguageModel(nn.Module):
 
     # positional embedding
     self.position_embedding_table = nn.Embedding(block_size, n_embed) # each position of 0 to block_size-1 will also be embedded
-    self.lm_head = nn.Linear(n_embed, vocab_size)
-    # self.sa_head = Head(n_embed) # Self Attention Head
-    self.sa_heads = MultiHeadAttention(4, n_embed//4)
-    # FFN
-    self.ffn = FFN(n_embed)
+    self.blocks = nn.Sequential(*[Block(n_embed, n_head=n_head) for _ in range(n_layers)])
+    # self.lm_head = nn.Linear(n_embed, vocab_size)
+    # # self.sa_head = Head(n_embed) # Self Attention Head
+    # self.sa_heads = MultiHeadAttention(4, n_embed//4)
+    # # FFN
+    # self.ffn = FFN(n_embed)
     self.lm_head = nn.Linear(n_embed, vocab_size)
 
   def forward(self, idx, targets=None):
@@ -133,8 +164,9 @@ class BigramLanguageModel(nn.Module):
     token_embeddings = self.token_embedding_table(idx) # (B, T, C=n_embed)
     position_embeddings = self.position_embedding_table(torch.arange(T, device=device)) # (T, C=n_embed)
     x = token_embeddings + position_embeddings # (B, T, C=n_embed)
-    x = self.sa_heads(x)
-    x = self.ffn(x)
+    # x = self.sa_heads(x)
+    # x = self.ffn(x)
+    x = self.blocks(x)
     logits = self.lm_head(x)
     if targets is None:
       loss = None
@@ -182,5 +214,7 @@ for steps in range(max_iters):
 
   loss.backward()
   optimizer.step()
-print(loss.item())
+out = estimate_loss()
+print(f"Final: Train Loss: {out['train']}, Eval Loss: {out['eval']}")
+print(decode(m.generate(idx=torch.zeros((1, 1), dtype=torch.long), max_new_tokens=100)[0].tolist()))
 
