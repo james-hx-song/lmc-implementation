08903d9ccebe2e664b2339293f497f3a23ab18a8
James Song <haoxijamessong@gmail.com>
2024-06-08 20:19:47 -0400

    Added helper for save/load checkpoints

--- utils.py
+++ utils.py
@@ -1,4 +1,43 @@
+import torch
+import os
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
+
+def interpolate_weights(model1, model2, baseline, alpha, device='cuda'):
+    interpolated_model = baseline.to(device)
+    state_dict1 = model1.state_dict()
+    state_dict2 = model2.state_dict()
+    interpolated_state_dict = {}
+
+    for key in state_dict1.keys():
+        interpolated_state_dict[key] = alpha * state_dict1[key] + (1 - alpha) * state_dict2[key]
+
+    interpolated_model.load_state_dict(interpolated_state_dict)
+    return interpolated_model
+
+def save_checkpoint(model, epoch, checkpoint_dir, optimizer=None):
+    checkpoint = {
+        'model': model.state_dict(),
+        'epoch': epoch,
+    }
+    if optimizer is not None:
+        checkpoint['optimizer'] = optimizer.state_dict()
+    filename = os.path.join(checkpoint_dir, "epoch={}.checkpoint.pth.tar".format(epoch))
+    torch.save(checkpoint, filename)
+
+def load_checkpoint(model, checkpoint_dir, optimizer=None):
+    checkpoint_files = os.listdir(checkpoint_dir)
+    if len(checkpoint_files) == 0:
+        return model, optimizer
+
+    checkpoint_files = sorted(checkpoint_files)
+    last_checkpoint = checkpoint_files[-1]
+    checkpoint = torch.load(os.path.join(checkpoint_dir, last_checkpoint))
+    model.load_state_dict(checkpoint['model'])
+
+    if optimizer is not None:
+        optimizer.load_state_dict(checkpoint['optimizer'])
+    
+    return model, optimizer
